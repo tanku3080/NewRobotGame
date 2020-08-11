586b94a6df79f8105f2565803172376f88dc1bac
WISTLE9999 <yamamoto0903@icloud.com>
2020-08-11 23:13:08 +0900

    no message

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform character, pivot;
+    [Range(-0.999f, -0.5f)] public float maxAngle = -0.5f;
+    [Range(0.5f, 0.999f)] public float minAngle = 0.5f;
+    void Start()
+    {
+        if (character == null) character = transform.parent;
+        if (pivot == null) pivot = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float X_Rotation = Input.GetAxis("Mouse X");
+        float Y_Rotation = Input.GetAxis("Mouse Y");
+        character.Rotate(0, X_Rotation, 0);
+
+        float nowAngle = pivot.localRotation.x;
+
+        if (-Y_Rotation != 0)
+        {
+            if (0 < Y_Rotation)
+            {
+                if (minAngle <= nowAngle) pivot.Rotate(-Y_Rotation, 0, 0);
+            }
+        }
+        else
+        {
+            if (nowAngle <= maxAngle) pivot.Rotate(-Y_Rotation, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    enum SceneChenge
+    {
+        _Title,_Game,_Result,_Sumple
+    }
+    SceneChenge _SneneChenge;
+    string Scenename;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void SceneLoader()
+    {
+        SceneManager.LoadScene(SneneSet());
+    }
+
+    string SneneSet()
+    {
+        SceneChenge chenge;
+        chenge = _SneneChenge;
+        switch (chenge)
+        {
+            case SceneChenge._Title:
+                Scenename = "Title";
+                break;
+            case SceneChenge._Game:
+                Scenename = "Game";
+                break;
+            case SceneChenge._Result:
+                Scenename = "Result";
+                break;
+            case SceneChenge._Sumple:
+                Scenename = "SampleScene";
+                break;
+        }
+        return SneneSet();
+    }
+}
