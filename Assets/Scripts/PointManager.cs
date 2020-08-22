326f05bebe59ff03241b4879d527407daaed63ab
tanku3080 <57029371+tanku3080@users.noreply.github.com>
2020-08-23 01:36:42 +0900

    作りかけのスクリプト

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    [Tooltip("味方の獲得ポイント")]
+    public int SidePoint;
+    [Tooltip("敵の獲得ポイント")]
+    public int enemyPoint;
+    [Tooltip("獲得ポイントの上限")]
+    public int limitPoint;
+    void Start()
+    {
+        //ここにplayerControllerから参照した得点を取得する
+    }
+
+    void Update()
+    {
+        //もし、どちらかの得点がlimitPointと同じかそれ以下ならPointMGを呼びだす
+        if (limitPoint <= SidePoint || limitPoint <= enemyPoint) 
+        {
+            PointMG();
+        }
+    }
+    /// <summary>ポイントを加算する処理を書く</summary>
+    void PointMG()
+    {
+        //ここの処理を書くならプレイヤーコントローラーを参照する。
+        //仮にプレイヤーコントローラーに書かれてなかったらテキトウな値をポイントとして加算するようにする
+    }
+}
