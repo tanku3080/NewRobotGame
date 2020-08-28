8816e6a674595e43fb958873a779096ea726d0f6
tanku3080 <57029371+tanku3080@users.noreply.github.com>
2020-08-28 17:39:39 +0900

    playerControllerとJumpControllerをマージしてる途中

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,12 +20,14 @@ public class PlayerController : MonoBehaviour
     int myLife = 20;
     public int point;
     public int maxLife = 20;
-    public float Speed = 2f,jump = 5f;
+    public float Speed = 8f,jump = 5f,jumpPower = 5f;
     [Tooltip("マズル")]
     public Transform _muzzleGun;
     public int RayDistance = 100;
     [Tooltip("体力の同期間隔")]
     public float lifeInterval = 1f;
+    //接地判定の際、中心(Pivot)からどれくらいの距離を「接地している」と判定するかの長さ
+    [SerializeField] float m_isGroundedLength = 1.1f;
     float timer;
     RaycastHit hit;
     Ray ray;
@@ -47,24 +49,25 @@ public class PlayerController : MonoBehaviour
 
         m_hpBar.maxValue = maxLife;
     }
-
-    private void Update()
+    void Update()
     {
+        // 方向の入力を取得し、方向を求める
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
         timer += Time.deltaTime;
+
         if (timer > lifeInterval)
         {
             myLife = maxLife;
             //hp処理を書く
             m_hpBar.value = myLife;
         }
-        float h = Input.GetAxis("Horizontal") * Speed;
-        float v = Input.GetAxis("Vertical") * Speed;
         float mouse = Input.GetAxis("Mouse ScrollWheel");
         int keep = (int)list2;
 
         if (mouse >= (int)WeponList.NULL) mouse = 0;
 
-        if (mouse > 0) 
+        if (mouse > 0)
         {
             keep++;
             Debug.Log("マウス前進");
@@ -74,22 +77,49 @@ public class PlayerController : MonoBehaviour
             keep--;
             Debug.Log("マウス後退");
         }
+        transform.Rotate(0, h * Speed, 0);
 
-        if (v > 0) animeSet(AnimeList.moveF);
-        else if (v < 0) animeSet(AnimeList.moveB);
-
-        transform.position = transform.position + new Vector3(h * timer, 0, v * timer);
+        if (Input.GetKey("w"))
+        {
+            animeSet(AnimeList.moveF);
+            transform.position += transform.forward * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            animeSet(AnimeList.moveB);
+            transform.position -= transform.forward * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= transform.right * Speed * Time.deltaTime;
+        }
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             ray = new Ray(_muzzleGun.position, _muzzleGun.forward);
 
-            if (Physics.Raycast(ray,out hit,RayDistance))
+            if (Physics.Raycast(ray, out hit, RayDistance))
             {
                 if (hit.collider.tag != "Pbject")
                 {
                     //Damage()
                 }
+            }
+        }
+
+        // ジャンプの入力を取得し、接地している時に押されていたらジャンプする
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rd.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+            // Animator Controller のパラメータをセットする
+            if (animator)
+            {
+                animator.SetBool("IsGrounded", false);
             }
         }
     }
@@ -100,6 +130,21 @@ public class PlayerController : MonoBehaviour
     //    //hpの関数を作って呼び出す
     //    object[] parameter = new object[] { myLife };
     //}
+
+
+
+    //地面に衝突しているか判定する
+    bool IsGrounded()
+    {
+        //Physics.Linecast()　を使って足元から線を張り、そこに何かが衝突していたら　true とする
+        CapsuleCollider col = GetComponent<CapsuleCollider>();
+        Vector3 start = this.transform.position + col.center;  // start: 体の中心
+        Vector3 end = start + Vector3.down * m_isGroundedLength; //end: start　から真下の地点
+        Debug.DrawLine(start, end); //動作確認用に Scene ウィンドウ上で線を表示する
+        bool isGrounded = Physics.Linecast(start, end); //引いたラインに何かがぶつかっていたら　true　とする
+        return isGrounded;
+    }
+
 
     void animeSet(AnimeList anime)
     {
